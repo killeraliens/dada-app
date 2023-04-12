8fb6d8672202a12061dacb5f820b681a73405e2c
killeraliens <killeraliens@outlook.com>
2023-04-11 19:46:28 -0700

    removed usage of EmailPasswordProvider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,19 +2,41 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextApiHandler } from "next";
 import { AuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
-import GitHubProvider from 'next-auth/providers/github';
 import prisma from "../../../lib/prisma";
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+
 export default authHandler;
 
 const options: AuthOptions = {
-    providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
-    ],
-    adapter: PrismaAdapter(prisma),
-    secret: process.env.SECRET,
+  providers: [],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+  callbacks: {
+    signIn: async (user, account, profile) => {
+      if (account.provider === 'email') {
+        const email = account.email;
+        const password = account.password;
+        // validate email and password here using your preferred method
+        // and return user object or false if validation fails
+        const isValid = await validateUser(email, password);
+        if (isValid) {
+          return Promise.resolve(user);
+        } else {
+          return Promise.resolve(false);
+        }
+      }
+    }
+  }
+};
+
+async function validateUser(email: string, password: string): Promise<boolean> {
+  // perform validation logic here, e.g. query the database for the user's credentials
+  // and return true if they match or false if they don't
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    return false;
+  }
+  const isValidPassword = true;
+  return isValidPassword;
 }
